4fcea08f6944266a8a58e932f99f2aa69feee88d
Phillip Oyediran <phillipoyediran@gmail.com>
2026-04-26 22:22:53 -0400

    Update frontend API URL for current ALB

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://techchallenge1-alb-1304935856.us-east-1.elb.amazonaws.com/api'
-export default API_URL
+export const API_URL = 'http://techchallenge1-alb-182052393.us-east-1.elb.amazonaws.com/api';
+
+export default API_URL;
